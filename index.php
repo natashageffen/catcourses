c06ef9015cc6e356d3363eef8bb583010ba593d0
Natasha R Geffen <natasha.geffen@uvm.edu>
2018-09-24 18:47:09 -0400

    modified index

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ if (DEBUG) {
     print '</pre></p>';
 }
 
-print '<h2 class="alternateRows">Meet the Jetsons!</h2>';
+print '<h2 class="alternateRows">Hiking Trails</h2>';
 if (is_array($records)) {
     foreach ($records as $record) {
         print '<p>' . $record['pmkTrailsId'] . ' ' . $record['fldTrailName'] . ' ' . $record['fldTotalDistance'] . ' ' . $record['fldHikingTime'] . ' ' . $record['fldVerticalRise'] . ' ' . $record['fldRating'] . '</p>';
